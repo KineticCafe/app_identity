80d3c143f4aebfe382299d52f96449b1420661ed
Austin Ziegler <aziegler@kineticcommerce.com>
2022-09-07 10:47:02 -0400

    WIP: Typedoc setup
    Signed-off-by: Austin Ziegler <aziegler@kineticcommerce.com>

--- ts/typedoc.js
+++ ts/typedoc.js
@@ -4,4 +4,31 @@ module.exports = {
   readme: 'README.md',
   sort: ['static-first', 'alphabetical'],
   excludeInternal: true,
+  pluginPages: {
+    pages: [
+      {
+        title: 'App Identity for Node.js',
+        children: [
+          { title: '', source: '../ts/spec.md' },
+          { title: 'App Identity Specification', source: '../spec/README.md' },
+          { title: 'Changelog', source: './Changelog.md' },
+          { title: 'Contributing', source: './Contributing.md' },
+          {
+            title: 'Licence',
+            source: './Licence.md',
+            children: [
+              {
+                title: 'Apache Licence, version 2.0',
+                source: './licenses/APACHE-20.txt',
+              },
+              {
+                title: 'Developer Certificate of Origin',
+                source: './licenses/dco.txt',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 }
