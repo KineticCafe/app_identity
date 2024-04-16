10a563e78bb7e3f3d09f5e35577772e3221749b8
Austin Ziegler <aziegler@kineticcommerce.com>
2024-04-16 23:29:11 +0100

    chore: Ensure Elixir 1.10 deps build clean
    When we move toward version 2.x, we will make Elixir 1.12 or later the
    minimum supported Elixir version, which will clean some of this up.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -52,13 +52,19 @@ defmodule AppIdentity.MixProject do
   defp deps do
     poison =
       if Version.compare(System.version(), "1.11.0") == :lt,
-        do: ">= 3.0.0 and < 6.0.0",
+        do: ">= 3.0.0 and < 5.0.0",
         else: ">= 3.0.0"
+
+    plug_crypto =
+      if Version.compare(System.version(), "1.11.0") == :lt,
+        do: "~> 1.2.5",
+        else: ">= 1.2.0"
 
     [
       {:jason, "~> 1.0", optional: true},
       {:plug, "~> 1.0", optional: true},
       {:poison, poison, optional: true},
+      {:plug_crypto, plug_crypto, optional: true},
       {:telemetry, "~> 0.4 or ~> 1.0", optional: true},
       {:tesla, "~> 1.0", optional: true}
     ] ++ dev_deps()
